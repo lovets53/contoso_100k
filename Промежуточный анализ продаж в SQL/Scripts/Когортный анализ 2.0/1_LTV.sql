e1da31ab83669f484f209828a61d603f899436df
Артем <lovets53@yandex.ru>
2026-03-03 13:46:38 +0400

    Добавлено представление в скрипты и изменен расчет ltv

--- Промежуточный анализ продаж в SQL/Scripts/Когортный анализ 2.0/1_LTV.sql
+++ Промежуточный анализ продаж в SQL/Scripts/Когортный анализ 2.0/1_LTV.sql
@@ -1,10 +1,11 @@
+
 --title:LTV на пользователя
 WITH cte AS (
 SELECT 
     cohort_year,
     count(distinct customerkey) AS count_customers,
     EXTRACT(YEAR FROM orderdate) - cohort_year AS lifetime_period,
-    ROUND(SUM(total_ner_revenue),2) AS period_revenue
+    ROUND(SUM(marginal_profit),2) AS period_profit
 FROM cogort_analysis
 GROUP BY 
     cohort_year,
@@ -13,7 +14,7 @@ GROUP BY
 SELECT 
     cohort_year,
     lifetime_period,
-    round(SUM(period_revenue) OVER(PARTITION BY cohort_year ORDER BY lifetime_period) / max(count_customers) OVER(PARTITION BY cohort_year),2) AS ltv_per_users
+    round(SUM(period_profit) OVER(PARTITION BY cohort_year ORDER BY lifetime_period) / max(count_customers) OVER(PARTITION BY cohort_year),2) AS ltv_per_users
 FROM cte
 ORDER BY 
     cohort_year,
@@ -25,7 +26,7 @@ WITH cte AS (
 SELECT 
     cohort_year,
     EXTRACT(YEAR FROM orderdate) - cohort_year AS lifetime_period,
-    ROUND(SUM(total_ner_revenue),2) AS period_revenue
+    ROUND(SUM(marginal_profit),2) AS period_profit
 FROM cogort_analysis
 GROUP BY 
     cohort_year,
@@ -34,7 +35,7 @@ GROUP BY
 SELECT 
     cohort_year,
     lifetime_period,
-    SUM(period_revenue) OVER(PARTITION BY cohort_year ORDER BY lifetime_period)  AS ltv_per_users
+    SUM(period_profit) OVER(PARTITION BY cohort_year ORDER BY lifetime_period)  AS ltv_per_users
 FROM cte
 ORDER BY 
     cohort_year,
---
+++ Промежуточный анализ продаж в SQL/Scripts/Создание представления.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE VIEW public.cogort_analysis
+AS WITH customer_revenue AS (
+         SELECT 
+         	s.customerkey,
+            s.orderdate,
+            sum(1.0*s.quantity * s.netprice / s.exchangerate) AS total_ner_revenue,
+            sum(1.0*s.quantity * s.netprice / s.exchangerate) - sum(1.0*s.quantity * s.unitcost/ s.exchangerate) AS marginal_profit,
+            count(s.orderkey) AS count_product,
+            c.countryfull,
+            c.age,
+            c.givenname,
+            c.surname
+           FROM sales s
+             LEFT JOIN customer c ON c.customerkey = s.customerkey
+          GROUP BY s.customerkey, s.orderdate, c.countryfull, c.age, c.givenname, c.surname
+        )
+ SELECT customerkey,
+    orderdate,
+    round(total_ner_revenue,2) AS total_ner_revenue,
+    round(marginal_profit,2) AS marginal_profit,
+    count_product,
+    countryfull,
+    age,
+    givenname,
+    surname,
+    min(orderdate) OVER (PARTITION BY customerkey) AS first_purchase_date,
+    EXTRACT(year FROM min(orderdate) OVER (PARTITION BY customerkey)) AS cohort_year
+   FROM customer_revenue;
